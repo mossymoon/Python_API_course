6cc5a8fb3e67f84ecf641c66d0eee87ff99fb23f
mossymoon <@stepanobuelo>
2024-06-22 22:10:06 +0300

    second_commit2

--- lib/base_case.py
+++ lib/base_case.py
@@ -14,7 +14,7 @@ class BaseCase:
     def get_json_value(self, response: Response, name):
         try:
             response_as_dict = response.json()
-        except json.decoder.JSONDecoderError:
+        except json.decoder.JSONDecodeError:
             assert False, f"Response is not in JSON format. Response text is '{response.text}'"
 
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,9 +1,32 @@
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from datetime import datetime
 
 
 class TestUserRegister(BaseCase):
+    def setup_email(self):
+        base_part = "learn_qa"
+        domain = "example.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{base_part}{random_part}@{domain}"
+
+    def test_create_user_successfully(self):
+        self.setup_email()
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': self.email
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+
+
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
         data = {
@@ -16,5 +39,5 @@ class TestUserRegister(BaseCase):
 
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
-        assert response.status_code == 400, f"Unexpected status code {response.status_code}"
+        Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected status code {response.content}"
